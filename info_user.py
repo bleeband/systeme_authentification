5205db81ac8b69d0c6ec6d8b97d7375fb5eed118
Pascal-the-dog <Mercierp@outlook.com>
2025-12-12 17:06:32 -0500

    Fix: Version 2 de info_user

--- info_user.py
+++ info_user.py
@@ -1,14 +1,10 @@
 
-sys_auth = []
+def DemandeInfo():
+   
+    pseudonyme=  input("Pseudonyme: ")
+    email = input("Email: ")
+    mot_de_passe = input("Mot de passe: ")
+ 
+    return {"pseudonyme": pseudonyme, "email": email, "mot_de_passe": mot_de_passe}
 
-
-# pseudonyme = input("Pseudonyme: ")
-# email = input("Adresse couriel: ")
-# mot_de_passe = input("Mot de passe: ")
-
-def DemandeInfo(pseudonyme, email, mot_de_passe):
-    sys_auth.append({"pseudonyme": pseudonyme,"email": email,"mot_de_passe": mot_de_passe})
-
-
-DemandeInfo(input("Pseudonyme: "), input("Email: "), input("Mot de passe: "))
-print(sys_auth)
+DemandeInfo()
