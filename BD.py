e0735aed1a1b78e38c441c7a94c8a810803e977f
Marc-André Dufour <MAD@nottrue.test>
2025-12-12 16:30:53 -0500

    fix: BD.py pwd pour mot_de_passe

--- BD.py
+++ BD.py
@@ -2,9 +2,9 @@
 # nom, prenom, email, username, mot_de_passe
 
 BD_sys_auth = [
-    {"nom": "Dufour", "prenom": "Marc", "email": "bleeband@gmail.com", "pseudonyme": "bleeband@gmail.com", "pwd": "1234"},
-    {"nom": "M.", "prenom": "Pascale", "email": "Mercierp@outlook.com", "pseudonyme": "Mercierp@outlook.com", "pwd": "1234"},
-    {"nom": "G.", "prenom": "Mathieu", "email": "math.gosselin@gmail.com", "pseudonyme": "math.gosselin@gmail.com", "pwd": "1234"},
-    {"nom": "L.", "prenom": "Clément", "email": "bb@bb.com", "pseudonyme": "bb@bb.com", "pwd": "1234"},
+    {"nom": "Dufour", "prenom": "Marc", "email": "bleeband@gmail.com", "pseudonyme": "bleeband@gmail.com", "mot_de_passe": "1234"},
+    {"nom": "M.", "prenom": "Pascale", "email": "Mercierp@outlook.com", "pseudonyme": "Mercierp@outlook.com", "mot_de_passe": "1234"},
+    {"nom": "G.", "prenom": "Mathieu", "email": "math.gosselin@gmail.com", "pseudonyme": "math.gosselin@gmail.com", "pwdmot_de_passe": "1234"},
+    {"nom": "L.", "prenom": "Clément", "email": "bb@bb.com", "pseudonyme": "bb@bb.com", "mot_de_passe": "1234"},
 
 ]
