a83e4db6bf81020d223bfc2b9bf596763c976892
Clement Laflamme <215926899+clementlaflamme@users.noreply.github.com>
2025-12-12 16:44:04 -0500

    Feature: check mot de passe valide et limite de tentatives

--- login.py
+++ login.py
@@ -1,20 +1,37 @@
 import BD
-import info_user
+# import info_user
 
-BD.BD_sys_auth
+nb_tentatives = 5
 
 def login():
 
-    #Recuperer infos user
-    infos = info_user.demander_infos()
-    
-
-    if info_user.pseudonyme or info_user.email not in BD.BD_sysauth:
-        print("Il n'y a aucun compte qui correspond à ces informations.")
+    #Instaurer une limite de tentatives
+    if nb_tentatives == 0:
+        print("Nombre de tentatives dépassé. Veuillez réessayer plus tard.")
         return False
+    
+    #Recuperer infos user
+    # infos = info_user.demander_infos()
+    infos = {"pseudonyme": "bb@bb.com", "email": "bb@bb.com", "mot_de_passe": "maoma"}
+    print(infos)
+
+    #Chercher si l'utilisateur existe    
+    for user in BD.BD_sys_auth:
+        if infos['pseudonyme'] == user['pseudonyme'] or infos['email'] == user['email']:
+            user_infos = user
+            print("Utilisateur trouvé :", user_infos)
+            break
+    else: 
+        print("Aucun utilisateur trouvé avec ces informations.")
+        return False
+            
     #Tester si le mot de passe est bon
-    if infos["mot_de_passe"] == BD.BD_sysauth[info_user.pseudonyme or info_user.email]["mot_de_passe"]:
+    if infos["mot_de_passe"] == user_infos['pwd']:
         print("Connexion réussie !")
         return True 
+    else:
+        print("Mot de passe incorrect.")
+        nb_tentatives -= 1
+        return False
 
-   
+print(login())
